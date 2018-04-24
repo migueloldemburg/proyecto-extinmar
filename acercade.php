9976d02bc4fb4ea5552d0a5cdaf1bcb55e47da4d
migueloldemburg <migueloldemburg2011@gmail.com>
2018-04-24 10:59:39 -0400

    nuevo

--- acercade.php
+++ acercade.php
@@ -14,12 +14,14 @@
 				<h3>ACERCA DE</h3>
 				<img src="images/unimar.jpg" style="width:40%; height:auto">
 				<img src="images/bg3.jpg" style="width:48%; height:auto">
-				<h4>TITULO DEL SISTEMA VA AQUI EN ESTE ESPACIO ESPECIALMENTE DISENADO PARA ESO Y ALGUN OTRA FUNCION</h4>
+				<h4>Sistema de información estratégico para el procesamiento de ordenes de servicio, búsqueda, entrega de recarga de extintores y gestión de químicos (BUSENEX). Para la empresa Seguridad Industrial Extinmar, C.A. Ubicada en la Av. Francisco Fajardo, local N°1, sector Conejeros a 200 metros del elevado de Conejeros, Municipio García, Estado Nueva Esparta</h4>
 				<br>
 				<p class="text-success">VERSI&Oacute;N 1.0.0</p>
 				<p class="text-muted">Lenguaje: PHP | Gestor de Base de Datos: MySQL</p>
 				<p class="text-success">AUTORES</p>
 				<p class="text-muted">Damaso Rodriguez</p>
+				<p class="text-muted">Mariana Marval</p>
+
 			</div>
 		</div>
 	</div>
--- enviacorreo.php
+++ enviacorreo.php
@@ -1,5 +1,5 @@
 <?php
 	include("class/Usuarios.php");
 	$user = new Usuarios();
-	$user->enviarRecordatorio("damaso_2011@hotmail.com");
+	$user->enviarRecordatorio("ORLANDA.TOCHON@UNIMAR.EDU.VE");
 ?>
